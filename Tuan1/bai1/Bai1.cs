287e8f7bf713aed8c2af923badee2e57839578ed
Phat <nguyenphat30032006@gmail.com>
2026-09-11 16:07:58 +0700

    Tuan01/Bai1/Nhap xuat ho ten

---
+++ Tuan1/bai1/Bai1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+
+
+namespace Tuan1
+{
+    public class Bai1
+    {
+        public static void Run()
+        {
+            Console.Write("Nhap ho va ten: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"\n Chao: {name}");
+        }
+    }
+
+
+}

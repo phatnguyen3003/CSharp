08ad65cd8486055632124122f70d1f893054d152
Phat <nguyenphat30032006@gmail.com>
2026-09-11 16:16:40 +0700

    Tuan01/Bai2/ Nhap ho va ten

--- Tuan1/Program.cs
+++ Tuan1/Program.cs
@@ -3,4 +3,5 @@
 
 using Tuan1;
 
-Bai1.Run();
+//Bai1.Run();
+Bai2.Run();
---
+++ Tuan1/bai2/bai2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+
+
+namespace Tuan1
+{
+    public class Bai2
+    {
+        public static void Run()
+        {
+            Console.Write("Nhap ho ten cua ban: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"\n Chao ban: {name}");
+        }
+    }
+
+
+}

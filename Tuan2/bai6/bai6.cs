cf45c30da74a3f43dadd26ff6ec9618186149515
Phat <nguyenphat30032006@gmail.com>
2026-09-23 21:42:53 +0700

    Tuan02/Bai6/Xay dung array point cho lop Point da dung o bai 2

---
+++ Tuan2/bai6/bai6.cs
@@ -0,0 +1,67 @@
+﻿using SharedLibs;
+using System;
+using System.Collections;
+
+namespace Tuan2
+{
+    public class ArrayPoint
+    {
+        private ArrayList points;   //field
+
+        public ArrayPoint() //constructor
+        {
+            points = new ArrayList();
+        }
+
+        public void Add(Point p)
+        {
+            points.Add(p);
+        }
+
+        public int count
+        {
+            get
+            {
+                return points.Count;
+            }
+        }
+
+
+        public Point this[int i]    //indexer
+        {
+            get
+            {
+                if (i < 0 || i >= points.Count)
+                {
+                    throw new IndexOutOfRangeException("Chi so vuot qua gioi han cua ArrayList");
+                }
+                return (Point)points[i];
+            }
+            set
+            {
+                if (i < 0 || i >= points.Count)
+                {
+                    throw new IndexOutOfRangeException("Chi so vuot qua gioi han cua ArrayList");
+                }
+                points[i] = value;
+            }
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayPoint arrayPoint = new ArrayPoint();
+
+            arrayPoint.Add(new Point(1, 2));
+            arrayPoint.Add(new Point(3, 4));
+
+            Point p1 = arrayPoint[0];
+            Console.WriteLine($"Point tại vị trí 0: X = {p1.X}, Y = {p1.Y}");
+
+            arrayPoint[1] = new Point(10, 20);
+            Console.WriteLine($"Point tại vị trí 1 sau khi sửa: X = {arrayPoint[1].X}, Y = {arrayPoint[1].Y}");
+        }
+    }
+}
